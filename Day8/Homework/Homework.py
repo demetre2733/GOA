076ce1bff2163906fdbb014404f6fbd04d40eea9
demetre2733 <146208605+demetre2733@users.noreply.github.com>
2023-12-11 11:54:44 +0400

    Add files via upload

---
+++ Day8/Homework/Homework.py
@@ -0,0 +1,8 @@
+family = ["tamuna", "saba", "mate"]
+ages = ["31", "18", "16"]
+ganaxlebuli_age = ["41", "28", "26"]
+full_sentence = "My moms name is: {}, My brothers name is: {}, My name is: {}".format(family[0],family[1],family[2])
+age_sentence = "My moms age is: {}, my brothers age is: {}, my age is: {}".format(ages[0],ages[1],ages[2])
+ganaxlebuli_age_sentence = "My mom in 10 years will be: {}, my brother in 10 years will be: {}, In 10 years I will be: {}".format(ganaxlebuli_age[0],ganaxlebuli_age[1],ganaxlebuli_age[2])
+print(full_sentence, age_sentence)
+print(ganaxlebuli_age_sentence)
---
+++ Day9/Homework/Homework.py
@@ -0,0 +1,6 @@
+def print_names(name1, name2, name3):
+    names_list = [name1, name2, name3]
+    names_str = ', '.join(names_list)
+    print(names_str)
+
+print_names("Tamuna", "Saba", "Sandro")
